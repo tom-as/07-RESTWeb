e0e48da980319137ac5664f418fedc8d7b3875ad
tom-as <arbiatoto@gmail.com>
2024-08-13 13:06:05 -0300

    Initial commit

---
+++ public/back/js/app.js
@@ -0,0 +1,2 @@
+
+console.log('Hola desde app.js');
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
+
+
+(()=> {
+    main();
+})();
+
+
+function main() {
+    console.log('main');
+
+    const server = new Server({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH,
+
+    });
+    server.start();
+}
